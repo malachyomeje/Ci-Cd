c3ea30fa1d99238d19aab5144498f8d2ed6d3aa8
malachy <malachyomeje@gmail.com>
2023-12-07 21:16:18 +0100

    registration comit

---
+++ src/main/java/com/cicd/service/serviceImp/UserServiceImp.java
@@ -0,0 +1,11 @@
+package com.cicd.service.serviceImp;
+
+import com.cicd.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImp implements UserService {
+}
